7eeee633629d4e046d117fc03648e31998dcf6a4
akimender <akimender@gmail.com>
2025-02-14 16:01:18 -0500

    changing text in About section

--- personal-website/src/components/About.jsx
+++ personal-website/src/components/About.jsx
@@ -12,7 +12,7 @@ const About = ({ isAboutVisible }) => {
             <h1>About Me</h1>
             <div class="about-text-container">
                 <p>
-                    Hi! I'm Andrew, a sophomore studying Applied Math + CS at Brown, and I'm developing my skills in software development, machine learning, and algorithmic problem solving! I'm passionate about applying my technical skills to real-world challenges and creating innovative solutions! I'm always curious to learn about applying mathematical techniques to overcoming challenges and creating cool stuff!
+                    Hi! I'm Andrew, a student studying Applied Math + Computer Science at Brown, and I'm developing my skills in software development, machine learning, and algorithmic problem solving! I'm passionate about applying my technical skills to real-world challenges and creating innovative solutions! I'm always curious to learn about applying mathematical techniques to overcoming challenges and creating cool stuff!
                 </p>
 
                 <img src="andrew-pose.jpg" alt="Andrew Kim" />
